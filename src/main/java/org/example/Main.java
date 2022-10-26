d98499ccc908c7cf0dfc4ff8e0d539310fc160ba
SpeedyBEAR777 <admin@primemotorcarrier.com>
2022-10-26 13:16:44 -0500

    local repo is already pushed to github

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello commit second time");
+
+        System.out.println("local repo is already pushed to github");
+
+        System.out.println("Doing more");
     }
 }
